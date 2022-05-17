3f32d8f55abfdb352d2bb31eb98fa0c88f0d440a
Arisandi Yudiarta <arisandiyudiarta21@gmail.com>
2022-05-17 22:24:42 +0800

    day 17, true or false quiz

---
+++ intermediate/true-or-false/main.py
@@ -0,0 +1,18 @@
+from question_model import Question
+from data import question_data
+from quiz_brain import QuizBrain
+
+question_bank = []
+for data in question_data:
+    questions = data["text"]
+    answer = data["answer"]
+    new_q = Question(questions, answer)
+    question_bank.append(new_q)
+
+question = QuizBrain(question_bank)
+
+while question.still_has_question():
+    question.next_q()
+print("Congratulation! You completed the entire quiz!")
+print(
+    f"Your final score is {question.user_score} out of {question.question_number}")
---
+++ intermediate/true-or-false/quiz_brain.py
@@ -0,0 +1,29 @@
+from locale import currency
+
+
+class QuizBrain:
+
+    def __init__(self, q_list) -> None:
+        self.question_number = 0
+        self.user_score = 0
+        self.question_list = q_list
+
+    def still_has_question(self):
+        return self.question_number < len(self.question_list)
+
+    def next_q(self):
+        current_q = self.question_list[self.question_number]
+        self.question_number += 1
+        users_answer = input(
+            f"Q.{self.question_number}: {current_q.question} your answer? (True/False) : ")
+        self.check_answer(users_answer, current_q.answer)
+
+    def check_answer(self, user_answer, correct_answer):
+        if user_answer.lower() == correct_answer.lower():
+            self.user_score += 1
+            print("You got it right!")
+        else:
+            print("Welp, you are wrong...")
+        print(f"The correct answer was: {correct_answer}")
+        print(
+            f"Your current score is {self.user_score}/{self.question_number}")
